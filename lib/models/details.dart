1ce162903bb26285b423a752061cda9c6de14a95
a5chin <81167661+a5chin@users.noreply.github.com>
2022-01-12 02:37:47 +0900

    Develop (#9)
    * create hexcolor class (#1)
    
    * create details.dart (#2)
    
    * create hexcolor class (#4)
    
    * create hexcolor class (#6)
    
    * Feature/details (#8)
    
    * create details.dart
    
    * Develop (#3)
    
    * create hexcolor class (#1)
    
    * create details.dart (#2)
    
    * Develop (#5)
    
    * create hexcolor class (#1)
    
    * create details.dart (#2)
    
    * create hexcolor class (#4)
    
    * create ColorList
    
    * modified Details class
    
    * modified design
    
    * add copy object
    
    Co-authored-by: Eigo Hara <hara.e@MD.LOCAL>
    
    Co-authored-by: Eigo Hara <hara.e@MD.LOCAL>

--- lib/models/details.dart
+++ lib/models/details.dart
@@ -1,35 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import './hexcolor.dart';
 
 class Details extends StatelessWidget {
-  Details({Key? key}) : super(key: key);
+  String color;
+  String name;
+
+  Details(this.color, this.name);
 
   @override
   Widget build(BuildContext context) {
     final Size _size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(),
-      body: Hero(
-        tag: '1',
-        child: Container(
-          height: _size.height / 5,
-          width: _size.width,
-          margin: const EdgeInsets.all(30),
-          decoration: BoxDecoration(
-            color: HexColor('895b8a'),
-            borderRadius: BorderRadius.circular(20.0),
-            boxShadow: [
-              BoxShadow(
-                color: HexColor('895b8a'),
-                spreadRadius: 10.0,
-                blurRadius: 30.0,
-                offset: const Offset(10, 10),
-              ),
-            ],
+        appBar: AppBar(),
+        body: ClipPath(
+          child: Container(
+            color: HexColor(color),
+            child: Column(
+              children: <Widget>[
+                Hero(
+                    tag: color,
+                    child: Padding(
+                        padding: EdgeInsets.only(top: _size.height / 6),
+                        child: Center(
+                          child: SelectableText.rich(
+                            TextSpan(children: <TextSpan>[
+                              TextSpan(
+                                  text: name,
+                                  style: GoogleFonts.getFont('Yuji Syuku',
+                                      fontSize: 50)),
+                              TextSpan(
+                                  text: ' (カラーコード: #$color)',
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ))
+                            ]),
+                          ),
+                        ))),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+          clipper: CustomClipPath(),
+        ));
   }
+}
+
+class CustomClipPath extends CustomClipper<Path> {
+  var radius = 10.0;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height / 5 * 2);
+    path.quadraticBezierTo(
+        size.width / 3, size.height / 2, size.width, size.height / 3);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
