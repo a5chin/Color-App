4accb9674464b0f3cd06547d4a6eb2cb508e2f5c
Eigo Hara <hara.e@MD.LOCAL>
2022-01-11 14:26:31 +0900

    create ColorList

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'models/hexcolor.dart';
-import 'models/details.dart';
+import 'models/colorlist.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,44 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
     List<String> _names = ['古代紫', '茄子紺', '二藍', '京紫', '蒲葡', '若紫'];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(children: <Widget>[
-          for (var i = 0; i < _colors.length; i++)
-            GestureDetector(
-              child: Hero(
-                tag: '1',
-                child: Container(
-                  height: _size.height / 6,
-                  width: _size.width,
-                  margin: const EdgeInsets.all(30),
-                  decoration: BoxDecoration(
-                    color: HexColor(_colors[i]),
-                    borderRadius: BorderRadius.circular(20.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: HexColor(_colors[i]),
-                        spreadRadius: 10.0,
-                        blurRadius: 30.0,
-                        offset: const Offset(10, 10),
-                      ),
-                    ],
-                  ),
-                  child: Center(child: Text(_names[i])),
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Details();
-                    },
-                  ),
-                );
-              },
-            ),
-        ]));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(children: <Widget>[
+        for (var i = 0; i < _colors.length; i++)
+          ColorList(_size.height, _size.width, _colors[i], _names[i])
+      ]));
   }
 }
---
+++ lib/models/colorlist.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import './hexcolor.dart';
+import '../models/details.dart';
+
+class ColorList extends StatelessWidget {
+  double height;
+  double width;
+  String color;
+  String name;
+
+  ColorList(this.height, this.width, this.color, this.name);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Hero(
+        tag: color,
+        child: Container(
+          height: height / 6,
+          width: width,
+          margin: const EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: HexColor(color),
+            borderRadius: BorderRadius.circular(20.0),
+            boxShadow: [
+              BoxShadow(
+                color: HexColor(color),
+                spreadRadius: 10.0,
+                blurRadius: 30.0,
+                offset: const Offset(10, 10),
+              ),
+            ],
+          ),
+          child: Center(child: Text(name)),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return Details();
+            },
+          ),
+        );
+      },
+    );
+  }
+}
